536ef574f4c6d6c078a532f44b9227e912279480
董兴龙 <ragnar@dongxinglongdeMacBook-Pro.local>
2019-09-04 23:15:53 +0800

    js数组部分

---
+++ jsdata.js
@@ -0,0 +1,85 @@
+const jsData = [{
+    title: 'Array对象方法',
+    list: [{
+        name: 'concat()',
+        intro: '连接两个或更多数组，并返回结果'
+    }, {
+        name: 'join()',
+        intro: '把数组的所有元素都放入一个字符串中。元素通过指定都分隔符进行分隔'
+    }, {
+        name: 'pop()',
+        intro: '删除并返回数组最后一个元素',
+        relevant: 'shift()'
+    }, {
+        name: 'push()',
+        intro: '向数组的末尾田间一个或更多元素'
+    }, {
+        name: 'reverse()',
+        intro: '颠倒数组的顺序',
+        relevant: 'soft()'
+    }, {
+        name: 'shift()',
+        intro: '删除并返回数组中第一个元素'
+    }, {
+        name: 'soft()',
+        intro: '对数组进行排序',
+        relevant: 'reverse()'
+    }, {
+        name: 'splice',
+        intro: '方法向/从数组中添加/删除项目，然后返回被删除的项目。'
+    }, {
+        name: 'toString()',
+        intro: '把数组转化为字符串并返回结果',
+        methods: 'Boolean 对象方法'
+    }, {
+        name: 'unshift()',
+        intro: '向数组的开头添加一个或更多元素，并返回新的长度'
+    }, {
+        name: 'valueOf()',
+        intro: '返回数组对象原始值',
+        methods: 'Boolean 对象方法'
+    }]
+}, {
+    title: 'Date对象方法',
+    list: [{
+        name: 'Date()',
+        intro: '返回当日日期和时间'
+    }, {
+        name: 'getDate()',
+        intro: '把数组的所有元素都放入一个字符串中。元素通过指定都分隔符进行分隔'
+    }, {
+        name: 'getDay()',
+        intro: '从date中取出（0～6）一周的某一天',
+    }, {
+        name: 'getMonth()',
+        intro: '从date获取月份 (0~11) 返回月份'
+    }, {
+        name: 'getFullYear()',
+        intro: '以四位数返回年份',
+    }, {
+        name: 'getHours()',
+        intro: '返回小时（0～23）'
+    }, {
+        name: 'getMinutes()',
+        intro: '返回Date对象的分钟(0~59)',
+    }, {
+        name: 'getSeconds()',
+        intro: '返回 Date 对象的秒数 (0 ~ 59)。'
+    }, {
+        name: 'toTimeString()',
+        intro: 'toTimeString() 返回例子22:58:54 GMT+0800 (中国标准时间)',
+        methods: 'Boolean 对象方法'
+    }, {
+        name: 'toDateString()',
+        intro: '根据本地时间格式，把 Date 对象转换为字符串 返回例子：2019/9/4 下午10:58:54'
+    }, {
+        name: 'toLocaleString()',
+        intro: '返回数组对象原始值',
+        methods: 'Boolean 对象方法'
+    }, {
+        name: 'toLocaleDateString()',
+        intro: '根据本地时间格式，把 Date 对象的日期部分转换为字符串 返回例子：2019/9/4'
+    }]
+}
+
+]
